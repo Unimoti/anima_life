f3b9678dccfe02987dab213d2e876b6e53a14373
polx2zo <polx2525@gmail.com>
2019-01-19 12:00:31 +0900

    anime2D Start !
    anime2D Start !

---
+++ anima_life/Assets/Scripts/PlayerHonyarara.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHonyarara : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string Akira = "アキラ";
+
+        int Suchi = 100;
+
+        string Pacnt = "%";
+
+        string GeyNin = Akira + Suchi + Pacnt;
+
+        Debug.Log(GeyNin);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
